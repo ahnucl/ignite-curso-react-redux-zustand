7d4cd308c35abdbc2e1dd67769ba93b5329798c8
Leonardo Cunha <lalmcunha@gmail.com>
2023-07-02 14:41:24 -0300

    Cria seletor para aula e modulo atual

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,6 +3,8 @@ import { Header } from "../components/Header";
 import { Module } from "../components/Module";
 import { Video } from "../components/Video";
 import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
+import { useEffect } from "react";
 
 export function Player() {
   const modules = useAppSelector(state => {
@@ -12,6 +14,12 @@ export function Player() {
 
     return modules
   })
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`
+  }, [currentLesson])
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
