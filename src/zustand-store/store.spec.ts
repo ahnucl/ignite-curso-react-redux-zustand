c49eaa844081a1bb755fa61441da39451524a365
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-07-07 16:55:41 -0300

    Cria testes para store do zustand

---
+++ src/zustand-store/store.spec.ts
@@ -0,0 +1,101 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { useStore as store } from '.' // como não pode ser usado como hook colocar outro nome
+
+const course = {
+  id: 1,
+  modules: [
+    {
+      id: 1,
+      title: 'Iniciando com React',
+      lessons: [
+        {
+          id: 'RNj5oxr7xfw',
+          title: 'Fundamentos do Redux',
+          duration: '09:"12',
+        },
+        {
+          id: 'w-DW4DhDfcw',
+          title: 'Estilização do Post',
+          duration: '10:05',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Estrutura da aplicação',
+      lessons: [
+        {
+          id: 'gE48FQXRZ_o',
+          title: 'Componente: Comment',
+          duration: '13:45',
+        },
+        { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+      ],
+    },
+  ],
+}
+
+const initialState = store.getState()
+
+describe('zustand store', () => {
+  beforeEach(() => {
+    store.setState(initialState)
+  })
+
+  it('should be able to play', () => {
+    const { play } = store.getState() // O Hook só funciona dentro de componentes react, mas
+    // dessa maneira é possível acessar os dados da store fora de um componente
+
+    play([1, 2])
+
+    const { currentLessonIndex, currentModuleIndex } = store.getState()
+
+    expect(currentModuleIndex).toEqual(1)
+    expect(currentLessonIndex).toEqual(2)
+  })
+
+  it('should be able to play next video automatically', () => {
+    store.setState({ course })
+
+    const { next } = store.getState()
+
+    next()
+
+    const { currentLessonIndex, currentModuleIndex } = store.getState()
+
+    expect(currentModuleIndex).toEqual(0)
+    expect(currentLessonIndex).toEqual(1)
+  })
+
+  it('should be able to jump to the next module automatically', () => {
+    store.setState({ course })
+
+    const { next } = store.getState()
+
+    store.setState({ currentLessonIndex: 1 })
+
+    next()
+
+    const { currentLessonIndex, currentModuleIndex } = store.getState()
+
+    expect(currentModuleIndex).toEqual(1)
+    expect(currentLessonIndex).toEqual(0)
+  })
+
+  it('should be not update current module and lesson index if there is no next lesson available', () => {
+    store.setState({
+      course,
+      currentLessonIndex: 1,
+      currentModuleIndex: 1,
+    })
+
+    const { next } = store.getState()
+
+    next()
+
+    const { currentLessonIndex, currentModuleIndex } = store.getState()
+
+    expect(currentModuleIndex).toEqual(1)
+    expect(currentLessonIndex).toEqual(1)
+  })
+})
